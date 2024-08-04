5a73f3a5c11a1792dfc17ca3ac785be4cd5562dc
poemsforaphrodite <pushpendersolanki_co21a6_23@dtu.ac.in>
2024-08-04 23:42:47 +0530

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <iframe
     src="https://ekkeltd-plagiarism-checker.hf.space"
     frameborder="0"
-    width="850"
-    height="450"
+    width="1200"
+    height="1200"
   ></iframe>
 </template>
